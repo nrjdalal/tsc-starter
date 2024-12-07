249314610d958e6edb3791698fa3cc8a3c3d7b63
Neeraj Dalal <admin@nrjdalal.com>
2024-12-07 10:45:07 +0530

    feat: init repo

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { generateId, type GenerateId } from "@/utils/generate-id"
+import { utcDate, type UtcDate } from "@/utils/utc-date"
+
+const date = utcDate()
+const id = generateId(date)
+
+console.log("\x1b[32m%s\x1b[0m", "Generated ID:", id)
+console.log("\x1b[32m%s\x1b[0m", "UTC Date:", date)
+
+export { generateId, type GenerateId, utcDate, type UtcDate }
